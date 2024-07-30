f4826a28214b94ca3f0317d208de3ef14f0b193a
Andrea Gulli <andreagulli11@gmail.com>
2024-07-30 15:23:43 +0200

    completed js (CONSOLE PART)

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// STEP 1 - Chiedere all'utente il suo nome 
+const userName = prompt('Inserisci il tuo nome');
+// Controllo input
+console.log(userName)
+
+// STEP 2 - chiedere il suo cognome
+const userNickname = prompt('Inserisci il tuo cognome')
+console.log(userNickname)
+
+// STEP 3 - chiedere il colore preferito
+const favColor = prompt('Inserisci il tuo colore preferito')
+console.log(favColor)
+
+// STEP 4 - stampare in console il nome, cognome, colore preferito ed il numero 21 senza spazi
+console.log(userName + userNickname + favColor + 21)
